8eb942ae511363244dde0679181981096033f0e5
Bouzgou <94169846+Bouzgou@users.noreply.github.com>
2022-11-07 00:18:44 +0100

    Update exo3.js

--- TP5/exo3.js
+++ TP5/exo3.js
@@ -15,3 +15,33 @@ for (let i = 0; i < process.argv.length - 3; i++) {
 if (check == 0) {
     console.log("NONE");
 }
+
+// question 2:
+
+var myvar = require('fs');
+var string = process.argv[2];
+
+function grep(list_of_files) {
+    for (let i = 0; i < list_of_files.length - 3; i++) {
+        if (myvar.lstatSync(list_of_files[i + 3]).isDirectory()) {
+            //console.log(true);
+            myvar.readdir(list_of_files[i + 3], (err, files) => {
+                myvar.readFile('./' + list_of_files[i + 3] + '/' + files[i], function (err, data) {
+                    if (err) throw err;
+                    if (data.includes(string)) {
+                        console.log(files[i]);
+                    }
+                });
+            });
+        } else {
+            myvar.readFile('./' + list_of_files[i + 3], function (err, data) {
+                if (err) throw err;
+                if (data.includes(string)) {
+                    console.log(list_of_files[i + 3]);
+                }
+            });
+        }
+    }
+}
+
+grep(process.argv)
