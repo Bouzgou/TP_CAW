fbfbd61c6e15837f2dc352a8686fd3f0f6c50b05
Bouzgou <94169846+Bouzgou@users.noreply.github.com>
2022-11-06 23:42:00 +0100

    Update exo3.js

--- TP5/exo3.js
+++ TP5/exo3.js
@@ -1,11 +1,17 @@
 var myvar = require('fs');
 var string = process.argv[2];
 
+let check = 0;
 for (let i = 0; i < process.argv.length - 3; i++) {
     myvar.readFile('./' + process.argv[i + 3], function (err, data) {
         if (err) throw err;
         if (data.includes(string)) {
+            check++;
             console.log(process.argv[i + 3]);
         }
     });
+}
+
+if (check == 0) {
+    console.log("NONE");
 }
